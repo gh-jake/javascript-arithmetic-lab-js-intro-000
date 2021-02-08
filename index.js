17c9fa5295d9406617b241f389899f3f9d2e962c
Jake Houston <jakechouston@gmail.com>
2021-02-08 00:52:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(num1, num2) {
+  return num1 += num2
+}
+
+function subtract(num1, num2) {
+  return num1 -= num2
+}
+
+function multiply(num1, num2) {
+  return num1 *= num2
+}
+
+function divide(num1, num2) {
+  return num1 /= num2
+}
+
+function increment(n) {
+  n++;
+  return n;
+}
+
+function decrement(n) {
+  
+}
